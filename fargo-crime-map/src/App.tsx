abf478371995909b91fc55c470e00ba673796a9f
Anel Drocic <anel.drocic@gmail.com>
2023-07-18 15:01:24 -0500

    Added react query configs, refactored types

--- fargo-crime-map/src/App.tsx
+++ fargo-crime-map/src/App.tsx
@@ -1,24 +1,28 @@
-import logo from "./logo.svg";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
+import ScrapingComponent from "Components/ScrapingComponent/scrapingComponent";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <ScrapingComponent />
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- fargo-crime-map/src/Components/ScrapingComponent/index.ts
+++ fargo-crime-map/src/Components/ScrapingComponent/index.ts
@@ -1,1 +1,1 @@
-export { default } from './scrapingComponent';
+export { default } from "./scrapingComponent";
