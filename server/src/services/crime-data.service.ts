2a03bd15a4aa785b92af8894a8ac1ae481cc0623
Anel Drocic <anel.drocic@gmail.com>
2023-07-26 12:06:46 -0500

    added interface for http response with crime data

--- server/src/services/crime-data.service.ts
+++ server/src/services/crime-data.service.ts
@@ -2,6 +2,15 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { CrimeDataProps } from '../modules/crime-data.module';
 
+interface CrimeDataServiceResponse {
+  status: number;
+  statusText: string;
+  headers: any; // You can create a specific type for AxiosHeaders if needed
+  config: any; // You can create a specific type for AxiosRequestConfig if needed
+  request: any; // You can create a specific type for ClientRequest if needed
+  data: CrimeDataProps[]; // Use the previously defined YourData interface here
+}
+
 @Injectable()
 export class CrimeDataService {
   async getCrimeData(
@@ -9,10 +18,10 @@ export class CrimeDataService {
     endDate = '7/25/2023',
   ): Promise<CrimeDataProps[]> {
     try {
-      const response: CrimeDataProps[] = await axios.get(
+      const response: CrimeDataServiceResponse = await axios.get(
         `https://fargond.gov/dispatchLogs?startDate=${startDate}&endDate=${endDate}`,
       );
-      return response;
+      return response.data;
     } catch (error) {
       // Handle error
       console.error('Error scraping crime data:', error);
