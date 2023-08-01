d66eb2e78ef1163a4176f4262dc7d7ed59533295
Anel Drocic <anel.drocic@gmail.com>
2023-07-31 22:33:05 -0500

    added custome theme.ts for chakra theme extension, restructured components, CSS, and added ViewSelector to change data viewing

--- fargo-crime-map/src/App.tsx
+++ fargo-crime-map/src/App.tsx
@@ -1,12 +1,13 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import JustData from "Views/JustData";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "theme";
 
 const App = () => {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <JustData />
       </ChakraProvider>
     </QueryClientProvider>
--- fargo-crime-map/src/Views/JustData/JustData.tsx
+++ fargo-crime-map/src/Views/JustData/JustData.tsx
@@ -1,14 +1,29 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex, Image, useTheme } from "@chakra-ui/react";
 import CrimeData from "Components/CrimeData";
 import MyDatePicker from "Components/MyDatePicker";
+import ViewSelector from "Views/ViewSelector/ViewSelector";
 
 const JustData = () => {
+  const theme = useTheme();
+
+  const handleSelection = (selection: "Data" | "Map") => {
+    // Handle the selection (e.g., render Data or Map component)
+    console.log(`Selected: ${selection}`);
+  };
+
   return (
     <Flex width="100vw" height="100vh" bg="white">
+      <Flex flexDir="column" width="100%">
+        <Flex justifyContent="center">
+          <Image height={500} src="/logo-color.svg" />
+        </Flex>
+        <ViewSelector onSelection={handleSelection} />
+        <Flex bg={theme.colors.secondary}>
+          <MyDatePicker />
+          <CrimeData />
+        </Flex>
+      </Flex>
       {/* eslint-disable-next-line */}
-      <Image height={500} src="/logo-color.svg" />
-      <MyDatePicker />
-      <CrimeData />
     </Flex>
   );
 };
